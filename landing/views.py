3059fb0eaf7394e0865c985ba8c0e78920d54d0e
Mirabror <99737165+0101mirabror@users.noreply.github.com>
2022-10-30 22:33:25 -0700

    homepage

--- landing/views.py
+++ landing/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView
+from core.models import Section
 
-# Create your views here.
+
+
+# class GetLanding(TemplateView):
+#     template_name = 'landing.html'
+
+class GetLanding(View):
+    def get(self, request, *args, **kwargs):
+        sections = Section.objects.all()
+        context = {
+            'sections': sections
+        }
+        template_name = 'landing.html'
+        return render(request, template_name, context)
